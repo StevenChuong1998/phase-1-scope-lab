714f9fd2c04786bc179d8d587dcda47650aab522
Steven <stevenchuong1998@gmail.com>
2023-05-07 22:33:54 -0600

    Help

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+return customerName
+
+var bestCustomer = 'not bob';
+return bestCustomer
+
+const leastFavoriteCustomer = 'Harry'
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase() 
+}
+
+function setBestCustomer() {
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    const bestCustomer= 'maybe bob'
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer(name) {
+    return leastFavoriteCustomer = `${name}`;
+    return leastFavoriteCustomer
+}
